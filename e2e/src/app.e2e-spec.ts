423f0abf3d67ca184ec9209e4954b048e32e6108
Jim Rasche <jjrasche@umich.edu>
2018-12-20 14:20:28 -0500

    Adding dist folder to use in Github Pages.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to graph!');
+    expect(page.getTitleText()).toEqual('Welcome to proSalAnalysis!');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'graph'`, () => {
+  it(`should have as title 'proSalAnalysis'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('graph');
+    expect(app.title).toEqual('proSalAnalysis');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to graph!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to proSalAnalysis!');
   });
 });
